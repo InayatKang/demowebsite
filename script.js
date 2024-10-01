9b476fe7df6c642525ac769f8faedac6b82de8d3
Inayat <114185464+InayatKang@users.noreply.github.com>
2024-09-30 19:30:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to open the modal
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close the modal if the user clicks outside the content
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
